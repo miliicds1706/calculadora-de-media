3bd06d9fa15f2ff2efe4d2b5e47ae5e72f047ecf
Miliicds1706 <Milenacsilva2010@gmail.com>
2026-03-04 11:22:59 -0300

    pegamos os dados

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("deu certo")
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+
+const button = document.getElementById("button")
+
+function calcularMedia(){
+    console.valornota1 = Number (nota1.value)
+     console.valornota2 = Number (nota2.value)
+
+     console media= (valorNota1+ValorNota2)/2
+
+     console.log(media)
+}
+
+button.addEventListener("click", calcularMedia)
